558bc65f58448df750b4cfd9875828de4ba4ebc7
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2024-02-06 15:56:39 -0500

    Update streamlit_app.py
    change file names to match py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,32 @@
 import streamlit as st
 import pandas as pd
 
-def load_data():
-    # Load the data from CSV file
-    data = pd.read_csv("teamstats.csv")
-    return data
+def load_team_data():
+    # Load the team data from CSV file
+    team_data = pd.read_csv("carolina_hurricanes_2006_team_stats.csv")
+    return team_data
+
+def load_player_data():
+    # Load the individual player data from CSV file
+    player_data = pd.read_csv("carolina_hurricanes_2006_player_stats.csv")
+    return player_data
 
 def main():
     st.title("Carolina Hurricanes 2006 Stanley Cup Winning Season Statistics")
     
-    # Load data
-    data = load_data()
+    # Load team data
+    team_data = load_team_data()
     
-    # Display the data
-    st.write("## Team Stats vs League Average")
-    st.write(data)
+    # Load player data
+    player_data = load_player_data()
+    
+    # Display team statistics
+    st.write("## Team Statistics")
+    st.write(team_data)
+    
+    # Display individual player statistics
+    st.write("## Individual Player Statistics")
+    st.write(player_data)
 
 if __name__ == "__main__":
     main()
